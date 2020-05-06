091361290d942de54b7af4a093d0c66859e765a9
Joao C Barbosa <joaao@outlook.com>
2020-05-06 20:23:19 -0300

    refactor: Add validators and sanitization to the Task model

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,10 +1,30 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    priority: Boolean,
-    done: Boolean
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (value.length > 800) {
+                throw new Error('Description must have until 800 characters!')
+            }
+        },
+    },
+    priority: {
+        type: Boolean,
+        required: true
+    },
+    done: {
+        type: Boolean,
+        required: true
+    },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
