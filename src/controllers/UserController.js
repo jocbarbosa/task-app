6bc614a548d5ab08a226943693a4aa6cb4c3859c
Joao C Barbosa <joaao@outlook.com>
2020-05-17 18:46:06 -0300

    feat: Finish users crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,8 +3,47 @@ const User = require('../models/User');
 
 module.exports = {
 
+    index(request, response) {
+        User.find()
+            .then(users => {
+                response.json(users);
+            })
+            .catch(error => {
+                response.status(500).json(error);
+            })
+    },
+
+    show(request, response) {
+        User.findById(request.params.id)
+            .then(user => {
+                response.json(user);
+            })
+            .catch(error => {
+                response.status(500).json(error);
+            })
+    },
+
+    update(request, response) {
+        User.findOneAndUpdate(request.params.id, request.body, { new: true })
+            .then((user) => {
+                response.json(user);
+            })
+            .catch(error => {
+                response.status(500).json(error);
+            })
+    },
+
     store(request, response) {
         User.create(request.body)
+            .then(user => {
+                response.status(201).json(user);
+            })
+            .catch(error => {
+                response.status(500).json(error);
+            });
+    },
+    async destroy(request, response) {
+        User.findOneAndDelete({ _id: request.params.id })
             .then(user => {
                 response.json(user);
             })
--- src/routes.js
+++ src/routes.js
@@ -14,9 +14,12 @@ routes.get('/tasks', TaskController.index);
 routes.put('/tasks/:id', TaskController.update);
 routes.delete('/tasks/:id', TaskController.destroy);
 routes.get('/tasks/pending', TaskController.pendingTasks);
-routes.get('/tasks/:id', TaskController.store);
 
 // User Routes
 routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.delete('/users/:id', UserController.destroy);
+routes.put('/users/:id', UserController.update);
 
 module.exports = routes;
