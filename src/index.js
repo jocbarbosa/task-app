e684a7d837c6e6ce19e0ca5a16a46b566497eae4
Joao C Barbosa <joaao@outlook.com>
2020-11-24 15:49:22 -0300

    feat: add maintenance mode middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -11,6 +11,14 @@ mongoose.connect(process.env.STRING_CONNECTION, {
 const routesTask = require('./routes/task');
 const routesAuth = require('./routes/auth');
 const routesUser = require('./routes/user');
+
+app.use((req, res, next) => {
+    if (process.env.MAINTENANCE === 'true') {
+        return res.status(503).json({ message: 'The service are on maintenance. Try later.' });
+    } else {
+        next();
+    }
+});
 
 app.use(express.json());
 app.use(routesTask);
